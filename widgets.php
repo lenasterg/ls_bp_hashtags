bea191728f69c3b5787230e1667bdad6055233ae
Lena Stergatou <stergatu@cti.gr>
2014-04-24 17:05:27 +0300

    Widget Current Group Hashtags

--- widgets.php
+++ widgets.php
@@ -3,7 +3,6 @@
 if ( ! defined( 'ABSPATH' ) )
     exit ;
 
-// Class wpb_widget ends here
 // Register and load the widget
 function ls_bp_hashtags_popular_widget_load() {
     register_widget( 'BP_Hashtags_Popular_Widget' ) ;
@@ -37,25 +36,21 @@ class BP_Hashtags_Popular_Widget extends WP_Widget {
                 // Base ID of your widget
                 'bp_hashtags_popular_widget' ,
                 // Widget name will appear in UI
-                __( 'Popular Hashtags' , 'bp-hashtags' ) , array ( 'description' => __( 'The most commonly used hashtags. Only for public activity' , 'bp-hashtags' ) ,
+                __( 'Popular Hashtags across network' , 'bp-hashtags' ) , array ( 'description' => __( 'The most commonly used hashtags. Only for public activity and from groups of logged in user.' , 'bp-hashtags' ) ,
             'classname' => 'bp_hashtags_widget' )
         ) ;
-//
-//        if ( is_active_widget( false , false , $this->id_base ) ) {
-//            add_action( '' , 'bp_group_documents_add_my_stylesheet' ) ;
-//        }
     }
 
     function widget( $args , $instance ) {
 
         $title = apply_filters( 'widget_title' , $instance[ 'title' ] ) ;
         if ( "" == $title ) {
-            $title = __( 'Popular Hashtags' , 'bp-hashtags' ) ;
+            $title = __( 'Popular Hashtags across network' , 'bp-hashtags' ) ;
         }
         // before and after widget arguments are defined by themes
         echo $args[ 'before_widget' ] ;
         if ( ! empty( $title ) ) {
-            echo $args[ 'before_title' ] . $title . $args[ 'after_title' ] ;
+            echo $args[ 'before_title' ] . sanitize_text_field( $title ) . $args[ 'after_title' ] ;
         }
         do_action( 'bp_hashtags_popular_widget_before_html' ) ;
         echo ls_bp_hashtags_generate_cloud() ;
@@ -68,7 +63,7 @@ class BP_Hashtags_Popular_Widget extends WP_Widget {
         if ( isset( $instance[ 'title' ] ) ) {
             $title = $instance[ 'title' ] ;
         } else {
-            $title = __( 'Popular Hashtags' , 'bp-hashtags' ) ;
+            $title = __( 'Popular Hashtags across network' , 'bp-hashtags' ) ;
         }
 // Widget admin form
         ?>
@@ -82,9 +77,64 @@ class BP_Hashtags_Popular_Widget extends WP_Widget {
 // Updating widget replacing old instances with new
     public function update( $new_instance , $old_instance ) {
         $instance = array () ;
-        $instance[ 'title' ] = ( ! empty( $new_instance[ 'title' ] ) ) ? strip_tags( $new_instance[ 'title' ] ) : '' ;
-    
-        return $instance ;
-    }
+        $instance[ 'title' ] = ( ! empty( $new_instance[ 'title' ] ) ) ? sanitize_text_field( $new_instance[ 'title' ] ) : '' ;
+                return $instance ;
+            }
 
 }
+
+/**
+ * Current group hashtag widget
+ * @version 1, 22/4/2014, stergatu
+ */
+class BP_Hashtags_CurrentGroup_Widget extends WP_Widget {
+
+    function __construct() {
+
+        parent::__construct(
+                // Base ID of your widget
+                'bp_hashtags_current_group_widget' ,
+                // Widget name will appear in UI
+                __( 'Hashtags in this group' , 'bp-hashtags' ) , array ( 'description' => __( 'The most commonly used hashtags in this group' , 'bp-hashtags' ) ,
+            'classname' => 'bp_hashtags_widget' )
+        ) ;
+    }
+
+    function widget( $args , $instance ) {
+        $bp = buddypress() ;
+        $instance[ 'group_id' ] = bp_get_current_group_id() ;
+
+        if ( $instance[ 'group_id' ] > 0 ) {
+            $group = $bp->groups->current_group ;
+            // If the group  public, or the user is super_admin or the user is member of group
+            if ( ($group->status == 'public') || (is_super_admin()) || (groups_is_user_member( bp_loggedin_user_id() , $group_id )) ) {
+                extract( $args ) ;
+                $title = apply_filters( 'widget_title' , empty( $instance[ 'title' ] ) ? sprintf( __( 'Hashtags from the group' , 'bp-group-documents' ) , $this->bp_group_documents_name ) : sanitize_text_field( $instance[ 'title' ] )  ) ;
+                        echo $before_widget . $before_title . $title . $after_title ;
+
+                        do_action( 'ls_bp_hashtags_current_group_widget_before_html' ) ;
+                        echo ls_bp_hashtags_generate_cloud() ;
+                        echo $after_widget ;
+                }
+            }
+        }
+
+        function update( $new_instance , $old_instance ) {
+            do_action( 'bp_hashtags_current_group_widget_update' ) ;
+                $instance = $old_instance ;
+            $instance[ 'title' ] = sanitize_text_field( $new_instance[ 'title' ] ) ;
+
+            return $instance ;
+        }
+
+        function form( $instance ) {
+            do_action( 'bp_hashtags_current_group_widget_form' ) ;
+                $title = sanitize_text_field( $instance[ 'title' ] ) ;
+                ?>
+
+        <p><label><?php _e( 'Title:' , 'bp-hashtags' ) ; ?></label><input class="widefat" id="<?php echo $this->get_field_id( 'title' ) ; ?>" name="<?php echo $this->get_field_name( 'title' ) ; ?>" type="text" value="<?php echo $title ; ?>" /></p>
+        <!--           <p><label><?php _e( 'Number of items to show:' , 'bp-group-documents' ) ; ?></label> <input class="widefat" id="<?php echo $this->get_field_id( 'num_items' ) ; ?>" name="<?php echo $this->get_field_name( 'num_items' ) ; ?>" type="text" value="<?php echo absint( $num_items ) ; ?>" style="width: 30%" /></p>-->
+                <?php
+        }
+
+    }
